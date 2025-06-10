d81a1118fc91b1e31d24571fcf6d93d56c79bf22
Val1t3 <valentinwoehrel2003@gmail.com>
2025-06-10 12:46:17 +0100

    feat(PlayerDetection): implement SAHI for video

--- src/player_detection_sahi.py
+++ src/player_detection_sahi.py
@@ -1,41 +1,87 @@
 from sahi import AutoDetectionModel
-from sahi.predict import get_prediction
 from sahi.predict import get_sliced_prediction
 from sahi.utils.cv import visualize_object_predictions
-from PIL import Image
+import cv2
 
 
-# Define constants
+# TODO:
+# [X] Retrieve result object list
+# [X] Filter with person category only
+# [X] Draw boxes and texts on the image
+# [ ] Check if box in the area of the court
+
+
+# constants
 asset = "assets/frame.png"
+video = "assets/extract-3.mp4"
 model = "models/yolov11n.pt"
+output = "output/player-detection-v11n.mp4"
+show = True
 
 
 if __name__ == '__main__':
-    # Create model detection
-    detection_model = AutoDetectionModel.from_pretrained(
+    # create model detection
+    model = AutoDetectionModel.from_pretrained(
         model_type="ultralytics",
         model_path=model,
         confidence_threshold=0.3,
         device="cpu",
     )
 
-    # Get width and height of the image
-    with Image.open(asset) as img:
-        width, height = img.size
-        print(f"Image width: {width}, height: {height}")
+    # open the input video
+    cap = cv2.VideoCapture(video)
 
-    # Predict using sliced prediction method
-    result = get_sliced_prediction(
-        asset,
-        detection_model,
-        slice_height=1075,
-        slice_width=1075,
-        overlap_height_ratio=0.2,
-        overlap_width_ratio=0.2,
-    )
+    # Video writer to save the output
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(output, fourcc, fps, (width, height))
 
-    # Access the object prediction list
-    object_prediction_list = result.object_prediction_list
+    # loop through frames
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # # Export result
-    result.export_visuals(export_dir="output/")
+        # predict using sliced prediction method
+        result = get_sliced_prediction(
+            frame,
+            model,
+            slice_height=800,
+            slice_width=800,
+            overlap_height_ratio=0.2,
+            overlap_width_ratio=0.2,
+            verbose=2,
+        )
+
+        # filter predictions to keep only "person" category
+        result.object_prediction_list = [
+            item for item in result.object_prediction_list
+            if item.category.name == "person"
+        ]
+
+        # draw on image detected persons
+        res = visualize_object_predictions(
+            image=frame,
+            object_prediction_list=result.object_prediction_list,
+            rect_th=2,
+            text_size=1,
+            text_th=1
+        )
+
+        # show frame (optional)
+        if show is True:
+            cv2.imshow("SAHI Player Detection", res["image"])
+
+        # write the frame to the output video
+        out.write(res["image"])
+
+        # press 'q' to exit early
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # release resources
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
