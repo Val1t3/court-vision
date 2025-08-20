469a250fd87d9311b6457fbf7f1a9ef52af7d6ac
Val1t3 <valentinwoehrel2003@gmail.com>
2025-08-20 10:49:37 +0200

    fix: modif titles in homography test

--- corpus/tracking/tracking_pipeline.sh
+++ corpus/tracking/tracking_pipeline.sh
@@ -2,6 +2,6 @@
 
 # execute python script for all videos
 for i in {1..11}; do
-	python3 main.py "../data/assets/eval_${i}.mov" --out-video ../data/courtvision-dataset/eval_${i}_tracked.mp4 --out-csv ../data/courtvision-dataset/eval_${i}_tracks.csv
+	python3 main.py "../data/courtvision-dataset/eval_${i}.mov" --out-video ../data/courtvision-dataset/eval_${i}_tracked.mp4 --out-csv ../data/courtvision-dataset/eval_${i}_tracks.csv
     echo "Finished processing eval_${i}.mov"
 done
